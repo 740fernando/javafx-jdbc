1f35c043b1e7a85ab480a684a3727cc694dfa1dc
Fernando <fernando.luiz92@hotmail.com>
2022-02-13 16:25:44 -0300

    Ação de inicialização como parâmetro (Consumer)

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -3,6 +3,7 @@ package gui;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.function.Consumer;
 
 import application.Main;
 import gui.util.Alerts;
@@ -32,13 +33,21 @@ public class MainViewController implements Initializable {
 	public void onMenuItemSellerAction() {
 		System.out.println("onMenuItemSellerAction");
 	}
+	
+	// ação de inicialização
+	//DepartmentListController controller = loader.getController(); // loader.getController() acessa o COntroller e instancia para DepartmentListController
+	// controller.setDepartmentService(new DepartmentService()); // processo manual de injeção de dependencia no controller
+	// controller.updateTableView(); //atualiza os dados na tela
 	@FXML
 	public void onMenuItemDepartmentAction() {
-		loadView2("/gui/DepartmentList.fxml");
+		loadView("/gui/DepartmentList.fxml",(DepartmentListController controller)->{
+			controller.setDepartmentService(new DepartmentService());
+			controller.updateTableView();// incluido segundo parametro, uma funcao lambda para inicializar o controlador, função de inicialização
+		});
 	}
 	@FXML
 	public void onMenuItemAboutAction() {
-		loadView("/gui/About.fxml");
+		loadView("/gui/About.fxml",x->{});// incluido segundo parametro, uma funcao lambda para inicializar o controlador, função vazia
 	}
 
 	@Override
@@ -47,8 +56,10 @@ public class MainViewController implements Initializable {
 	/*
 	 * synchronized - Garente que o processamento não será interrompido por algum comportamento multithread
 	 * loadView - método responsável por manipular a scena principal
+	 * 
+	 * Consumer - Interface funcional
 	 */
-	private synchronized void loadView(String absoluteName) {
+	private synchronized <T> void loadView(String absoluteName, Consumer<T> initializingAction) {
 		
 		try {
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
@@ -76,44 +87,15 @@ public class MainViewController implements Initializable {
 			//Adiciona a coleção - Os filhos do new Vbox
 			mainVBox.getChildren().addAll(newVBox.getChildren());
 			
-		} catch (IOException e) {
-			Alerts.showAlert("IO Exception","Error loading view", e.getMessage(), AlertType.ERROR);
-		}
-	}
-private synchronized void loadView2(String absoluteName) {
-		
-		try {
-			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoluteName));
-			VBox newVBox = loader.load();
+			// executa a função passada no argumento - inicio
+			T controller = loader.getController(); // vai retornar o controlador do tipo que eu chamar 
+			initializingAction.accept(controller);
+			// executa a função passada no argumento - fim
 			
-			// Pega a referencia da scene do main
-			Scene mainScene = Main.getMainScene();
-			
-			
-			//getRoot() - obtem o primeiro elemento da view(ScrolLPane);
-			//getContent() - Dentro do MainView.fxml, tem um elemento chamado content, é utilizado esse método para pode acessar
-			VBox mainVBox =(VBox) ((ScrollPane) mainScene.getRoot()).getContent(); // Obtém a referencia do scroll pane e joga para o Vbox
-			
-			//Vou guardar uma referencia para o menu
-			//getChildren() - obtém os Filhos do Vbox
-			//get(0) - Primeiro filho do Vbox da janela principal(mainMenu)
-			Node mainMenu = mainVBox.getChildren().get(0); 
-			
-			//limpa todos os filhos do VBox
-			mainVBox.getChildren().clear();
-			
-			//Adiciona o menu
-			mainVBox.getChildren().add(mainMenu);
-			
-			//Adiciona a coleção - Os filhos do new Vbox
-			mainVBox.getChildren().addAll(newVBox.getChildren());
-			
-			DepartmentListController controller = loader.getController(); // loader.getController() acessa o COntroller e instancia para DepartmentListController
-			controller.setDepartmentService(new DepartmentService()); // processo manual de injeção de dependencia no controller
-			controller.updateTableView(); //atualiza os dados na tela
 			
 		} catch (IOException e) {
 			Alerts.showAlert("IO Exception","Error loading view", e.getMessage(), AlertType.ERROR);
 		}
 	}
+
 }
