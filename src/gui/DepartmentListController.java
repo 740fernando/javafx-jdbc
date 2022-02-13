931ec0e173e44ff3012ce8447d31e32f39437618
Fernando <fernando.luiz92@hotmail.com>
2022-02-13 15:55:39 -0300

    DepartmentService

--- src/gui/DepartmentListController.java
+++ src/gui/DepartmentListController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import application.Main;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -12,9 +15,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 import model.entities.Department;
+import model.services.DepartmentService;
 
 public class DepartmentListController implements Initializable {
 
+	private DepartmentService service;
+	
 	@FXML
 	private TableView<Department> tableViewDepartment;
 
@@ -27,9 +33,15 @@ public class DepartmentListController implements Initializable {
 	@FXML
 	private Button btNew;
 
+	private ObservableList<Department> obsList; // Os departamentos são carregados aqui
+	
 	@FXML
 	public void onBtNewAction() {
 		System.out.println("onBtNewAction");
+	}
+	
+	public void setDepartmentService(DepartmentService service) {
+		this.service = service;
 	}
 
 	@Override
@@ -44,9 +56,19 @@ public class DepartmentListController implements Initializable {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
 		
-		// macete para o tableViewDepartment ser exibido até  o final da janela - inio
+		// macete para o tableViewDepartment ser exibido até  o final da janela - inicio
 		Stage stage = (Stage) Main.getMainScene().getWindow();//referencia do stage atual, getWindow() é super classe do stage, é nessário fazer o downcast para atribuir para o stage
 		tableViewDepartment.prefHeightProperty().bind(stage.heightProperty());
-		// macete para o tableViewDepartment ser exibido até  o final da janela - inio
+		// macete para o tableViewDepartment ser exibido até  o final da janela - fim
+	}
+	//Esse método vai ser responsável por acessar os serviços, carregar os departamentos  e jogar para obsList, aí eu vou associar meu obsList ao meu tableView, que exibirá as informações
+	public void updateTableView() {
+		// teste - Verifica se o service foi instanciado
+		if(service==null) {
+			throw new IllegalStateException("Service está nulo");
+		}
+		List<Department> list = service.findAll();
+		obsList = FXCollections.observableArrayList(list);// Instancia o obsList recebendo os dados do list
+		tableViewDepartment.setItems(obsList); // carrega as informaçoes contidas no obsList para o tableView e exibe na tela
 	}
 }
