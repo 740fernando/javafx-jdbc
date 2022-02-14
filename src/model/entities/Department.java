6eeb1ae0c081a3e2fedbdc3215a964630e5087b0
Fernando <fernando.luiz92@hotmail.com>
2022-02-14 08:29:21 -0300

    modified Department

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,21 +1,26 @@
 package model.entities;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 public class Department implements Serializable {
 
+	/**
+	 * implements Serializable - Permite que os objetos possam ser transformados em
+	 * sequencia de byte. Na linguagem Java,é necessário implements Serializable
+	 * para que nosso objeto seja gravado em arquivo, redes e etc
+	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
-	
-	public Department() {	
+
+	public Department() {
+
 	}
-	
+
 	public Department(Integer id, String name) {
-		this.id=id;
-		this.name=name;
+		this.id = id;
+		this.name = name;
 	}
 
 	public Integer getId() {
@@ -36,9 +41,14 @@ public class Department implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(id);
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
 	}
 
+	// M�todo criado para que os objetos sejam comparados pelo conte�do e n�o pela
+	// ref�rencia de ponteiros
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
@@ -48,6 +58,16 @@ public class Department implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Department other = (Department) obj;
-		return Objects.equals(id, other.id);
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Department [id=" + id + ", name=" + name + "]";
 	}
 }
