026b73659ed823009db9ae3dcf662523c3b57ea8
Fernando <fernando.luiz92@hotmail.com>
2022-03-08 08:34:28 -0300

    Padrão de projeto Observer para atualizar a TableView

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -1,9 +1,12 @@
 package gui;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 
 import db.DbException;
+import gui.listeners.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Constraints;
 import gui.util.Utils;
@@ -22,6 +25,8 @@ public class DepartmentFormController implements Initializable {
 	private Department entity;
 	
 	private DepartmentService service;
+	
+	private List<DataChangeListener> dataChangeListeners = new ArrayList<>(); //Armazena os eventos
 	
 	@FXML
 	private TextField txtId;
@@ -45,6 +50,13 @@ public class DepartmentFormController implements Initializable {
 	public void setDepartmentService(DepartmentService service) {
 		this.service=service;
 	}
+	/**
+	 * adiciona o evento
+	 * @param listener
+	 */
+	public void subscribeDataChangeListener(DataChangeListener listener) {
+		dataChangeListeners.add(listener);
+	}
 	
 	@FXML
 	public void onBtSaveAction(ActionEvent event) {
@@ -57,12 +69,22 @@ public class DepartmentFormController implements Initializable {
 		try {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
+			notifyDataChangeListeners();
 			Utils.currentStage(event).close();;
 		}catch(DbException e) {
 			Alerts.showAlert("Erro ao salver o objeto", null, e.getMessage(), AlertType.ERROR);
 		}
 			
 	}
+	/**
+	 * Responsável por emetir o evento
+	 */
+	private void notifyDataChangeListeners() {
+		for(DataChangeListener listener : dataChangeListeners) {
+			listener.onDataChanged();
+		}
+	}
+
 	/**
 	 * Responsavel por pegar os dados nas caixinhas do formulario, instanciar o department
 	 * 
---
+++ src/gui/listeners/DataChangeListener.java
@@ -0,0 +1,15 @@
+package gui.listeners;
+
+/**
+ * Interface responsável por permite um objeto escutar de outro objeto.
+ * 
+ * @author Fernando
+ *
+ */
+public interface DataChangeListener {
+
+	/**
+	 * Evento ao ser disparado quando houver alteraçoes nos dados 
+	 */
+	void onDataChanged();
+}
