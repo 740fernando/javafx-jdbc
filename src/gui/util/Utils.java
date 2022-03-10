fea2b5ef8c1f46a161702ca3106d886846ec170d
Fernando <fernando.luiz92@hotmail.com>
2022-03-10 20:46:54 -0300

    SellerList Tableview

--- src/gui/util/Utils.java
+++ src/gui/util/Utils.java
@@ -1,26 +1,91 @@
 package gui.util;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 import javafx.event.ActionEvent;
 import javafx.scene.Node;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
-/**
- * Classe responsável por receber o stage a partir do objeto de evento
- * 
- * @author Fernando
- *
- */
+
 public class Utils {
 
+	/**
+	 * Método responsável por receber o stage a partir do objeto de evento
+	 * 
+	 * @author Fernando
+	 *
+	 */
 	public static Stage currentStage(ActionEvent event) {
 		return (Stage) ((Node) event.getSource()).getScene().getWindow();
 	}
 
+	/**
+	 * Tenta converter para inteiro, caso não consiga retorna nulo;
+	 * 
+	 * @param str
+	 * @return
+	 */
 	public static Integer tenteConverterParaInt(String str) {
 		try {
 			return Integer.parseInt(str);
 		} catch (NumberFormatException e) {
 			return null;
 		}
+	}
+	/**
+	 * Formata a Data
+	 * 
+	 * @param <T>
+	 * @param tableColumn
+	 * @param format
+	 */
+
+	public static <T> void formatTableColumnDate(TableColumn<T, Date> tableColumn, String format) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Date> cell = new TableCell<T, Date>() {
+				private SimpleDateFormat sdf = new SimpleDateFormat(format);
+
+				@Override
+				protected void updateItem(Date item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						setText(sdf.format(item));
+					}
+				}
+			};
+			return cell;
+		});
+	}
+
+	/**
+	 * Formata o número com ponto flutuante.
+	 * 
+	 * @param <T>
+	 * @param tableColumn
+	 * @param decimalPlaces
+	 */
+	public static <T> void formatTableColumnDouble(TableColumn<T, Double> tableColumn, int decimalPlaces) {
+		tableColumn.setCellFactory(column -> {
+			TableCell<T, Double> cell = new TableCell<T, Double>() {
+
+				@Override
+				protected void updateItem(Double item, boolean empty) {
+					super.updateItem(item, empty);
+					if (empty) {
+						setText(null);
+					} else {
+						Locale.setDefault(Locale.US);
+						setText(String.format("%." + decimalPlaces + "f", item));
+					}
+				}
+			};
+			return cell;
+		});
 	}
 }
